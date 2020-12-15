0fcbc3f2e50cf172b3057cf9d14179bd7cb5bcbd
va barbosa <va@us.ibm.com>
2020-12-15 17:11:34 -0500

    component location

--- textbook-ui/gulpfile.js
+++ textbook-ui/gulpfile.js
@@ -8,32 +8,37 @@ const replace = require('rollup-plugin-replace');
 const {nodeResolve} = require('@rollup/plugin-node-resolve');
 
 function ui() {
-  return gulp.src(['components/components.js'])
-      .pipe(rollup({plugins: [
-        replace({
-          'process.env.NODE_ENV': JSON.stringify( 'production' )
-        }),
-        vue({
-          compileTemplate: true,
-          style: {
-            preprocessOptions: {
-              scss: {
-                includePaths: ['node_modules'],
-                implementation: require('node-sass'),
-                data: '@import "components/scss/mq.scss"; @import "components/scss/mixins.scss";'
+  return gulp.src(['src/components.js'])
+      .pipe(
+        rollup({plugins: [
+          replace({
+            'process.env.NODE_ENV': JSON.stringify( 'production' )
+          }),
+          vue({
+            compileTemplate: true,
+            style: {
+              preprocessOptions: {
+                scss: {
+                  includePaths: ['node_modules'],
+                  implementation: require('node-sass'),
+                  data: '@import "src/scss/mq.scss"; @import "src/scss/mixins.scss";'
+                }
               }
             }
-          }
-        }),
-        nodeResolve(),
-        typescript()]},
-                   {format: 'iife'}))
+          }),
+          nodeResolve(),
+          typescript()
+        ]},
+        {
+          format: 'iife'
+        })
+      )
       .pipe(rename('bundle.js'))
       .pipe(gulp.dest('build'));
 }
 
 exports.watch = () => {
-  gulp.watch('components/**/*.{js,ts,scss}', ui);
+  gulp.watch('src/**/*.{js,ts,scss}', ui);
 };
 
 exports.default = gulp.parallel(ui);
