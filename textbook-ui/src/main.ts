7adfb469833be835b3567cb29e5ab24f9066658d
Gregorio <korgan00@gmail.com>
2021-02-08 13:15:31 +0100

    added layers circuit as exported component

--- textbook-ui/src/main.ts
+++ textbook-ui/src/main.ts
@@ -1,7 +1,9 @@
-import TheMenu from "./components/TheMenu.vue";
-import WhatIsQuantumVolume from "./components/WhatIsQuantumVolume/WhatIsQuantumVolume.vue";
+import TheMenu from './components/TheMenu.vue'
+import WhatIsQuantumVolume from './components/WhatIsQuantumVolume/WhatIsQuantumVolume.vue'
+import LayersCircuit from './components/WhatIsQuantumVolume/LayersCircuit.vue'
 
 export default {
   TheMenu,
-  WhatIsQuantumVolume
+  WhatIsQuantumVolume,
+  LayersCircuit
 }
