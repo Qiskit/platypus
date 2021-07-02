377b04263a79fb7915258f9d81161165a0601f31
va barbosa <va@us.ibm.com>
2021-07-01 22:22:12 -0400

    update textbook beta url

--- server/app.ts
+++ server/app.ts
@@ -6,7 +6,8 @@ import { MathigonStudioApp } from '@mathigon/studio/server/app'
 import { getCourse } from '@mathigon/studio/server/utilities'
 
 import {
-  CONFIG, COURSES, NOTATIONS, UNIVERSAL_NOTATIONS, findNextSection, findPrevSection, getSectionIndex, isLearningPath, updateGlossary
+  CONFIG, COURSES, NOTATIONS, TEXTBOOK_HOME, UNIVERSAL_NOTATIONS,
+  findNextSection, findPrevSection, getSectionIndex, isLearningPath, updateGlossary
 } from './utilities'
 import * as storageApi from './storage'
 
@@ -14,8 +15,10 @@ new MathigonStudioApp()
   .get('/health', (req, res) => res.status(200).send('ok'))  // Server Health Checks
   .secure()
   .setup({sessionSecret: 'project-platypus-beta'})
-  .redirects({ '/': '/textbook' })
-  .get('/textbook', (req, res) => res.render('index.pug', { courses: COURSES }))
+  .redirects({
+    '/': TEXTBOOK_HOME,
+    '/textbook': TEXTBOOK_HOME
+  })
   .get('/course/:course/:section', async (req, res, next) => {
     const course = getCourse(req.params.course, req.locale.id)
     const section = course?.sections.find(s => s.id === req.params.section)
@@ -37,7 +40,7 @@ new MathigonStudioApp()
       course, section, config: CONFIG,
       progressJSON, notationsJSON, learningPath,
       nextSection, prevSection, universalJSON,
-      subsections
+      subsections, textbookHome: TEXTBOOK_HOME
     });
   })
   .course(storageApi)
