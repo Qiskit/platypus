4d216c0d43c3e278d194723fe592a6690028d840
Gregorio Iniesta <korgan00@gmail.com>
2021-07-21 14:03:28 +0200

    cypress installed and few introductory course tests

---
+++ cypress/integration/courses/intro/layout.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+describe('Layout Macbook-15', () => {
+  beforeEach(() => {
+    cy.viewport('macbook-15')
+      .visit('http://localhost:8080/course/introduction/why-quantum-computing')
+
+      .setCookie('cmapi_cookie_privacy', 'permit_1|2|3')
+      .setCookie('cmapi_gtm_bl', '')
+      .setCookie('notice_gdpr_prefs', '0|1|2:')
+      .setCookie('notice_preferences', '2:')
+
+      .reload()
+  })
+
+  it('Navigation sidebar', () => {
+    cy.get('.c-sidebar__chapter').should('be.visible')
+
+    cy.get('#app-panel-footer-toggle').click()
+
+    cy.get('.c-sidebar__chapter').should('not.be.visible')
+  })
+
+  it('Glossary side bar', () => {
+    cy.get('.utility-panel-content').should('be.visible')
+
+    cy.get('.utility-panel-header__toggle').click()
+
+    cy.get('.utility-panel-content').should('not.be.visible')
+  })
+
+  it('Browse all content', () => {
+    cy.get('.app-mega-dropdown').should('be.visible')
+    cy.get('.app-mega-dropdown__content-container').should('not.exist')
+
+    cy.get('.app-mega-dropdown').click()
+
+    cy.get('.app-mega-dropdown').should('be.visible')
+    cy.get('.app-mega-dropdown__content-container').should('be.visible')
+  })
+})
