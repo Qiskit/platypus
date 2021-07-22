44caa047799682e27b9dcee98435a37b7960ec73
Gregorio Iniesta <korgan00@gmail.com>
2021-07-22 12:17:03 +0200

    improved code quality

--- cypress/integration/courses/intro/layout.spec.js
+++ cypress/integration/courses/intro/layout.spec.js
@@ -3,8 +3,9 @@
 describe('Layout Macbook-15', () => {
   beforeEach(() => {
     cy.viewport('macbook-15')
-      .visit('http://localhost:8080/course/introduction/why-quantum-computing')
+      .visit('/course/introduction/why-quantum-computing')
 
+      // set cookies to prevent IBM privacy policy popup
       .setCookie('cmapi_cookie_privacy', 'permit_1|2|3')
       .setCookie('cmapi_gtm_bl', '')
       .setCookie('notice_gdpr_prefs', '0|1|2:')
@@ -13,35 +14,41 @@ describe('Layout Macbook-15', () => {
       .reload()
   })
 
-  it('should hide then show navigation sidebar', () => {
+  it('Should hide then show navigation sidebar', () => {
+    // initial state
     cy.get('.c-sidebar__chapter').should('be.visible')
 
+    // hide
     cy.get('#app-panel-footer-toggle').click()
-
     cy.get('.c-sidebar__chapter').should('not.be.visible')
+
+    // restore initial state
+    cy.get('#app-panel-footer-toggle').click()
+    cy.get('.c-sidebar__chapter').should('be.visible')
   })
 
-  it('should hide then show utility panel', () => {
+  it('Should hide then show utility panel', () => {
+    // initial state
     cy.get('.utility-panel-content').should('be.visible')
 
+    // hide
     cy.get('.utility-panel-header__toggle').click()
-
     cy.get('.utility-panel-content').should('not.be.visible')
+
+    // restore initial state
+    cy.get('.utility-panel-header__toggle').click()
+    cy.get('.utility-panel-content').should('be.visible')
   })
 
-  it('should open Browse all content', () => {
+  it('Should open Browse all content', () => {
+    // initial state
     cy.get('.app-mega-dropdown').should('be.visible')
     cy.get('.app-mega-dropdown__content-container').should('not.exist')
 
+    // show
     cy.get('.app-mega-dropdown').click()
 
     cy.get('.app-mega-dropdown').should('be.visible')
     cy.get('.app-mega-dropdown__content-container').should('be.visible')
-  })
-
-  it('Navigate to atoms of computation', () => {
-    cy.url().should('include', '/introduction/why-quantum-computing')
-    cy.get('[data-section-id="the-atoms-of-computation"]').click()
-    cy.url().should('include', '/introduction/the-atoms-of-computation')
   })
 })
