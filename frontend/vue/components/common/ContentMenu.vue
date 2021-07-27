c87e3ba55e0d9e42e4bf9074b6c5fa74d5d8fca0
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-07-27 14:58:46 +0200

    refactor(mega-menu-dropdown): use `qiskit-vue` component

--- frontend/vue/components/common/ContentMenu.vue
+++ frontend/vue/components/common/ContentMenu.vue
@@ -1,37 +1,44 @@
 <template>
   <div>
-    <AppMegaDropdownMenu
+    <QiskitMegaMenuDropdown
+      class="content-menu__mega-menu-dropdown"
       :content="dropdownMenuContent"
       segment-component-name="Textbook mega menu"
+      :track-performed-search="trackPerformedSearch"
     />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import { MegaMenuDropdown as QiskitMegaMenuDropdown } from '@qiskit-community/qiskit-vue'
 import { TEXTBOOK_DEMO_MEGA_MENU } from '../constants/megaMenuLinks'
-import AppMegaDropdownMenu from './AppMegaDropdownMenu.vue'
 
 @Options({
   components: {
-    AppMegaDropdownMenu
+    QiskitMegaMenuDropdown
   }
 })
 export default class ContentMenu extends Vue {
   dropdownMenuContent = TEXTBOOK_DEMO_MEGA_MENU
+
+  trackPerformedSearch (...params: Array<[string, string]>) {
+    const windowInstance = (window as any)
+    this.trackPerformedSearch = windowInstance.textbook.trackPerformedSearch(...params)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~@carbon/colors/scss/colors';
 
-// Qiskit variables
-$background-color-lighter: $cool-gray-10;
-$border-color: $cool-gray-20;
+.content-menu {
+  &__mega-menu-dropdown {
+    background: $cool-gray-10;
+  }
+}
 
-.content-menu-section {
-  background: $background-color-lighter;
-  border-top: 1px solid $border-color;
-  border-bottom: 1px solid $border-color;
+:deep(.app-mega-dropdown__filter-wrapper) {
+  padding-left: 24px;
 }
 </style>
