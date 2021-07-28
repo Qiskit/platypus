1984c01a20f2b30091d56d9558d1512c26627d58
randytolentino <randytolentino@ibm.com>
2021-07-28 13:43:02 -0700

    update dropdown item colors

--- frontend/vue/components/common/LanguageSelector.vue
+++ frontend/vue/components/common/LanguageSelector.vue
@@ -3,10 +3,11 @@
     <bx-dropdown
       class=""
       :trigger-content="`English`"
+      :open="true"
     >
-      <bx-dropdown-item class="language-selector__item" value="test">Test</bx-dropdown-item>
-      <bx-dropdown-item class="language-selector__item" value="test">Test</bx-dropdown-item>
-      <bx-dropdown-item class="language-selector__item" value="test">Test</bx-dropdown-item>
+      <bx-dropdown-item class="language-selector__item" value="test">English</bx-dropdown-item>
+      <bx-dropdown-item class="language-selector__item" value="test">Japanese</bx-dropdown-item>
+      <bx-dropdown-item class="language-selector__item language-selector__item-disabled" value="test">Japanese disabled</bx-dropdown-item>
     </bx-dropdown>
   </div>
 </template>
@@ -23,19 +24,30 @@ export default class LanguageSelector extends Vue {}
 @import '../../../scss/variables/colors.scss';
 
 .language-selector {
+  border-top: 1px solid #dde1e6;
+  border-bottom:1px solid #dde1e6;
   &__item {
     text-align: left;
     padding-left: 1rem;
+    background-color: $border-color-light;
+
+    &-disabled {
+      color: $border-color;
+    }
   }
+
 }
 </style>
 
 <style lang="scss">
 @import '../../../scss/variables/colors.scss';
+@import 'carbon-components/scss/globals/scss/typography';
+// @import '~/../scss/variables/mq.scss';
+// @import '~/../scss/mixins/mixins.scss';
+// @import '~/../scss/variables/colors.scss';
 // overrides
 q-language-selector {
   width: 100%;
-  border-top: 1px solid $border-color;
 }
 
 .language-selector {
@@ -45,7 +57,7 @@ q-language-selector {
     background-color: $background-color-lighter;
     justify-content: flex-end;
     display: flex;
-    padding-left: 2rem;
+    padding-left: $spacing-07;
   }
 
   bx-dropdown::part(menu-body) {
