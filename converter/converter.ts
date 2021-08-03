d8f199b80375ffb115bf51de8392cbccaea1f3e5
y4izus <yaiza.garcia.mm@gmail.com>
2021-08-03 12:37:02 +0200

    Refactor

--- converter/converter.ts
+++ converter/converter.ts
@@ -7,18 +7,17 @@ import { load as loadYAML } from 'js-yaml'
 const CWD = process.cwd()
 
 // Get the languages to translate from the mathigon config file or throw an error
-let translationsLanguages
+let translationsLanguages: Array<string>
 
 try {
   const mathigonConfigFile: any = loadYAML(fs.readFileSync(`${CWD}/config.yaml`, 'utf8'))
   const textbookLanguages = mathigonConfigFile.locales
   translationsLanguages = textbookLanguages.filter(language => language !== 'en')
 } catch (e) {
-  console.log(e)
+  console.error(e)
 }
 
 const nbImagesDirName = 'images'
-
 const sharedContent = `${CWD}/shared`
 const nbDir = `${CWD}/notebooks`
 const nbDirTranslations = `${CWD}/notebooks/translations/`
@@ -39,7 +38,6 @@ const runConverter = function (
   console.log('textbook converter', arguments)
 
   // TODO: replace converter Python implementation with a Node.js implementation
-  // python3 -u -m textbook_converter /Users/yaiza/Documents/development/platypus/notebooks/toc-ja.yaml -n /Users/yaiza/Documents/development/platypus/notebooks/translations/ja -o /Users/yaiza/Documents/development/platypus/translations
   return spawn('python3', [
     '-u', '-m',
     'textbook_converter', tocPath,
@@ -50,20 +48,23 @@ const runConverter = function (
   })
 }
 
-const copyNotebookAssets = function(
+const copyNotebookAssets = function (
   srcDir: string, destDir: string, filterFunc: CallableFunction
 ) {
   console.log(`textbook converter: Copying assets from ${srcDir}`)
-  fs.copySync(srcDir, destDir, { filter: (src: string, dest: string) => {
-    const name = path.basename(src)
-    if (name.startsWith('.')) {
-      return false
-    } else if (fs.statSync(src).isDirectory()) {
-      return true
-    } else {
-      return filterFunc(src, dest)
+
+  fs.copySync(srcDir, destDir, {
+    filter: (src: string, dest: string) => {
+      const name = path.basename(src)
+      if (name.startsWith('.')) {
+        return false
+      } else if (fs.statSync(src).isDirectory()) {
+        return true
+      } else {
+        return filterFunc(src, dest)
+      }
     }
-  }})
+  })
 }
 
 // const convertNotations = async function() {
@@ -81,7 +82,9 @@ copyNotebookAssets(nbDir, publicContentDir, (src: string, dest: string) => {
 })
 
 const subprocess = runConverter(getTOCPath(), nbDir, workingDir)
-translationsLanguages.forEach(language => runConverter(getTOCPath(language), `${nbDirTranslations}${language}`, `${translationsDir}${language}`))
+translationsLanguages.forEach(language =>
+  runConverter(getTOCPath(language), `${nbDirTranslations}${language}`, `${translationsDir}${language}`)
+)
 
 subprocess.stdout.on('data', (data) => {
   console.log(`${data}`)
