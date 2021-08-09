cf243929252b5c3f40794fc53eb1eb468501d1ad
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-08-09 14:52:41 +0200

    test(cta): next and previous section links work

---
+++ cypress/integration/cta.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+  cy
+    .viewport('macbook-15')
+    .setCookie('cmapi_cookie_privacy', 'permit_1|2|3')
+    .setCookie('cmapi_gtm_bl', '')
+    .setCookie('notice_gdpr_prefs', '0|1|2:')
+    .setCookie('notice_preferences', '2:')
+})
+
+describe('CTA', () => {
+  it('next and previous section links work', () => {
+    cy
+      .visit('/course/ch-prerequisites')
+      .get('[data-cy=footer-nav-next-section]').click()
+
+    cy.url().should(
+      'include',
+      '/course/ch-prerequisites/introduction-to-python-and-jupyter-notebooks'
+    )
+    cy.get('[data-cy=footer-nav-previous-section]').click()
+
+    cy.url().should(
+      'include',
+      '/course/ch-prerequisites/environment-setup-guide-to-work-with-qiskit-textbook'
+    )
+  })
+})
