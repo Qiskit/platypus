c7835aa6e2b923a22358abce1a29eacdb2d27f58
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-08-09 13:44:49 +0200

    test: don't show privacy notice pop-up

--- cypress/integration/all-pages.spec.js
+++ cypress/integration/all-pages.spec.js
@@ -99,8 +99,13 @@ describe('Every page', () => {
   pagesUrls.forEach((pageUrl) => {
     it('loads', () => {
       cy
+        .setCookie('cmapi_cookie_privacy', 'permit_1|2|3')
+        .setCookie('cmapi_gtm_bl', '')
+        .setCookie('notice_gdpr_prefs', '0|1|2:')
+        .setCookie('notice_preferences', '2:')
+
         .viewport('macbook-15')
-        .visit(pageUrl)
+        .visit(pageUrl, { pageLoadTimeout: 3000 })
     })
   })
 })
