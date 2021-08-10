df9e72330062d248cd5e61a8cfec5a465bf0b162
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-08-10 15:11:52 +0200

    test: custom "visit" CY command

--- cypress/integration/code-mirror/code-cell.js
+++ cypress/integration/code-mirror/code-cell.js
@@ -2,16 +2,7 @@
 
 describe('Code cell', () => {
   beforeEach(() => {
-    cy.viewport('macbook-15')
-      .visit('/course/examples/blocks')
-
-      // set cookies to prevent IBM privacy policy popup
-      .setCookie('cmapi_cookie_privacy', 'permit_1|2|3')
-      .setCookie('cmapi_gtm_bl', '')
-      .setCookie('notice_gdpr_prefs', '0|1|2:')
-      .setCookie('notice_preferences', '2:')
-
-      .reload()
+    cy.viewport('macbook-15').visit('/course/examples/blocks')
   })
 
   it('is displayed', () => {
