75b3e33118d600a2410dc92980c04c2eb7273004
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-08-12 14:51:31 +0200

    test: all pages load

--- cypress/integration/all-pages.spec.js
+++ cypress/integration/all-pages.spec.js
@@ -1,38 +1,22 @@
 /// <reference types="cypress" />
 
-const urls = []
+describe('All pages', () => {
+  const sitemapJson = require('../../public/sitemap.json')
+  const sitemapUrlsDefaultBase = 'qiskit.org'
 
-before(() => {
-  cy.readFile('public/sitemap.xml').then((sitemapAsString) => {
-    const domParser = new DOMParser()
+  const urls = sitemapJson.urlset.url.map(({ loc }) => {
+    const url = loc[0]
 
-    const sitemapParsed = domParser.parseFromString(
-      sitemapAsString,
-      'application/xml'
+    const relativeUrl = url.substring(
+      url.indexOf(sitemapUrlsDefaultBase) + sitemapUrlsDefaultBase.length
     )
 
-    const sitemapUrls = sitemapParsed.getElementsByTagName('loc')
-
-    for (const sitemapUrl of sitemapUrls) {
-      const sitemapUrlsDefaultBase = 'qiskit.org'
-      const url = sitemapUrl.innerHTML
-
-      const relativeUrl = url.substring(
-        url.indexOf(sitemapUrlsDefaultBase) + sitemapUrlsDefaultBase.length
-      )
-
-      urls.push(relativeUrl)
-    }
+    return relativeUrl
   })
-})
 
-describe('Every page', () => {
-  it('loads', () => {
-    urls.forEach((url) => {
-      cy.request('GET', url, { responseTimeout: 3000 })
-        .then((response) => {
-          expect(response.status).to.eq(200)
-        })
+  urls.forEach((url) => {
+    it(`should load ${url}`, () => {
+      cy.visit(url)
     })
   })
 })
