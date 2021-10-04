c0ca024e9d494c019b684679c8c19d37b6f41e0a
Gregorio Iniesta <korgan00@gmail.com>
2021-10-04 16:28:12 +0200

    refactor

---
+++ frontend/vue/components/UserAccount/ChapterProgress.vue
@@ -0,0 +1,72 @@
+<template>
+  <BasicLink
+    class="chapter-progress"
+    :url="chapter.url"
+  >
+    <x-progress
+      class="chapter-progress__progress-indicator"
+      r="8"
+      :p="chapter.progress"
+    />
+    <span>{{ chapter.title }}</span>
+  </BasicLink>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+import BasicLink from '../common/BasicLink.vue'
+
+export default defineComponent({
+  name: 'ChapterProgress',
+  components: {
+    BasicLink
+  },
+  props: {
+    chapter: {
+      type: Object,
+      required: false,
+      default: () => {
+        return {
+          title: '1. Setting Up Your Environment',
+          url: '/course/introduction/why-quantum-computing',
+          progress: 1
+        }
+      }
+    }
+  },
+  data () {
+    return { }
+  },
+  computed: {
+  },
+  mounted () {
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '../../../../node_modules/carbon-components/scss/globals/scss/layout';
+@import '../../../../node_modules/carbon-components/scss/globals/scss/typography';
+@import '~/../scss/variables/mq.scss';
+@import '~/../scss/variables/colors.scss';
+@import '~/../scss/variables/fonts.scss';
+@import '~/../scss/mixins/mixins.scss';
+
+.chapter-progress {
+  @include type-style('body-long-01');
+  padding: $spacing-04 $spacing-04 $spacing-04 0;
+  display: flex;
+  align-items: center;
+  // this is to make x-progress render properly
+  z-index: 0;
+  color: $text-color-black;
+
+  &:hover {
+    color: $text-active-color;
+  }
+
+  &__progress-indicator {
+    margin-right: $spacing-03;
+  }
+}
+</style>
--- frontend/vue/components/UserAccount/CourseProgress.vue
+++ frontend/vue/components/UserAccount/CourseProgress.vue
@@ -7,31 +7,24 @@
       class="course-progress__chapter-list"
       :style="`--row-count-l: ${Math.ceil(course.chapters.length / 3)}; --row-count-m: ${Math.ceil(course.chapters.length / 2)}; --row-count-s: ${course.chapters.length};`"
     >
-      <BasicLink
+      <ChapterProgress
         v-for="(chapter, index) in course.chapters"
         :key="index"
         class="course-progress__chapter"
-        :url="chapter.url"
-      >
-        <x-progress
-          class="course-progress__chapter__progress"
-          r="8"
-          :p="chapter.progress"
-        />
-        <span>{{ chapter.title }}</span>
-      </BasicLink>
+        :chapter="chapter"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue-demi'
-import BasicLink from '../common/BasicLink.vue'
+import ChapterProgress from './ChapterProgress.vue'
 
 export default defineComponent({
   name: 'CourseProgress',
   components: {
-    BasicLink
+    ChapterProgress
   },
   props: {
     course: {
@@ -91,37 +84,20 @@ export default defineComponent({
     padding: $spacing-04 0;
   }
 
-  &__chapter {
-    @include type-style('body-long-01');
-    padding: $spacing-04 $spacing-04 $spacing-04 0;
-    display: flex;
-    align-items: center;
-    // this is to make x-progress render properly
-    z-index: 0;
-    color: $text-color-black;
+  &__chapter-list {
+    --column-count:3;
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(var(--column-count), 1fr);
+    grid-template-rows: repeat(var(--row-count-l, 3), 1fr);
 
-    &:hover {
-      color: $text-active-color;
+    @include mq($from: medium, $until: large) {
+      --column-count:2;
+      grid-template-rows: repeat(var(--row-count-m, 5), 1fr);
     }
-
-    &-list {
-      --column-count:3;
-      display: grid;
-      grid-auto-flow: column;
-      grid-template-columns: repeat(var(--column-count), 1fr);
-      grid-template-rows: repeat(var(--row-count-l, 3), 1fr);
-
-      @include mq($from: medium, $until: large) {
-        --column-count:2;
-        grid-template-rows: repeat(var(--row-count-m, 5), 1fr);
-      }
-      @include mq($until: medium) {
-        --column-count:1;
-        grid-template-rows: repeat(var(--row-count-s, 9), 1fr);
-      }
-    }
-    &__progress {
-      margin-right: $spacing-03;
+    @include mq($until: medium) {
+      --column-count:1;
+      grid-template-rows: repeat(var(--row-count-s, 9), 1fr);
     }
   }
 }
