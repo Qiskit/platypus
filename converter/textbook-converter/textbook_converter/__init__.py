bd28831818c746835e5b7d37d76e8e0bf823537d
Frank Harkins <frankharkins@hotmail.co.uk>
2021-10-05 10:44:26 +0100

    remove unneeded files

--- converter/textbook-converter/textbook_converter/__init__.py
+++
@@ -1,2 +0,0 @@
-from .TextbookExporter import TextbookExporter, mathigon_ximg_regex, html_img_regex
-from .converter import *
--- frontend/plugins/i18nStrings.ts
+++
@@ -1,9 +0,0 @@
-import { translate } from '../ts/translations'
-
-const install = function (app: any, options?: any) {
-  app.config.globalProperties.$translate = (str: string, args?: string[]) => translate(str, args, options?.translations)
-}
-
-export {
-  install
-}
