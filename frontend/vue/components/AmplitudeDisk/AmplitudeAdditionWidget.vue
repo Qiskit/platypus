8b364d09e01b39e7329e879364ce7ad14ddaaf93
Gregorio Iniesta <korgan00@gmail.com>
2021-11-05 16:46:32 +0100

    Added amplitudeAdditionDisk and widget

---
+++ frontend/vue/components/AmplitudeDisk/AmplitudeAdditionWidget.vue
@@ -0,0 +1,124 @@
+<template>
+  <div
+    class="amplitude-addition-widget"
+  >
+    <AmplitudeDiskWithValues
+      class="amplitude-addition-widget__disk amplitude-addition-widget__disk_a"
+      :magnitude="internalAmplitudeA.magnitude"
+      :phase="internalAmplitudeA.phase"
+      :is-interactive="true"
+      @updateAmplitude="updateInternalAmplitudeA"
+    />
+    <span class="amplitude-addition-widget__math-sign">+</span>
+    <AmplitudeDiskWithValues
+      class="amplitude-addition-widget__disk amplitude-addition-widget__disk_b"
+      :magnitude="internalAmplitudeB.magnitude"
+      :phase="internalAmplitudeB.phase"
+      :is-interactive="true"
+      @updateAmplitude="updateInternalAmplitudeB"
+    />
+    <span class="amplitude-addition-widget__math-sign">=</span>
+    <AmplitudeAdditionDiskWithValues
+      class="amplitude-addition-widget__disk amplitude-addition-widget__disk_result"
+      :magnitude-a="internalAmplitudeA.magnitude"
+      :phase-a="internalAmplitudeA.phase"
+      :magnitude-b="internalAmplitudeB.magnitude"
+      :phase-b="internalAmplitudeB.phase"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+import AmplitudeDiskWithValues from './AmplitudeDiskWithValues.vue'
+import AmplitudeAdditionDiskWithValues from './AmplitudeAdditionDiskWithValues.vue'
+import { Amplitude } from './amplitude'
+
+export default defineComponent({
+  name: 'AmplitudeAdditionDisk',
+  components: {
+    AmplitudeAdditionDiskWithValues,
+    AmplitudeDiskWithValues
+  },
+  props: {
+    magnitudeA: {
+      type: Number,
+      required: false,
+      default: 0.8
+    },
+    phaseA: {
+      type: Number,
+      required: false,
+      default: 30
+    },
+    magnitudeB: {
+      type: Number,
+      required: false,
+      default: 0.6
+    },
+    phaseB: {
+      type: Number,
+      required: false,
+      default: 190
+    }
+  },
+  data () {
+    return {
+      internalAmplitudeA: { phase: 30, magnitude: 1 },
+      internalAmplitudeB: { phase: 30, magnitude: 1 }
+    }
+  },
+  mounted () {
+    this.updateInternalAmplitudeA({ phase: this.phaseA, magnitude: this.magnitudeA } as Amplitude)
+    this.updateInternalAmplitudeB({ phase: this.phaseB, magnitude: this.magnitudeB } as Amplitude)
+  },
+  methods: {
+    updateInternalAmplitudeA (amplitude: Amplitude) {
+      this.internalAmplitudeA = { phase: amplitude.phase, magnitude: Math.min(amplitude.magnitude, 1.1) }
+    },
+    updateInternalAmplitudeB (amplitude: Amplitude) {
+      this.internalAmplitudeB = { phase: amplitude.phase, magnitude: Math.min(amplitude.magnitude, 1.1) }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import 'carbon-components/scss/globals/scss/typography';
+@import '~/../scss/variables/colors.scss';
+@import '~/../scss/variables/mq.scss';
+
+.amplitude-addition-widget {
+  display: flex;
+  flex-flow: row;
+  gap: $spacing-04;
+
+  @include mq($until: medium) {
+    flex-flow: column;
+  }
+
+  &__math-sign {
+    @include type-style('expressive-heading-06');
+    display: flex;
+    align-items: center;
+    height: 100px;
+    margin-top: $spacing-05;
+
+    @include mq($until: medium) {
+      display: block;
+      margin-top: 0;
+      height: auto;
+      width: 100px;
+      margin-left: $spacing-05;
+      text-align: center;
+    }
+  }
+
+  &__disk {
+    @include mq($until: medium) {
+      --display-direction: row;
+    }
+  }
+}
+
+</style>
