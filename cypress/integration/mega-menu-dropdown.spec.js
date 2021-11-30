4bd64ec8f26117d1547c66793eb87b9b0613e975
va barbosa <va@us.ibm.com>
2021-11-30 14:22:22 -0500

    test links

--- cypress/integration/mega-menu-dropdown.spec.js
+++ cypress/integration/mega-menu-dropdown.spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 
 describe('Mega menu dropdown', () => {
+  const originRegex = /(.*?:\/\/.*?\/)/
+  const sitemapJson = require('../../public/sitemap.json')
+  const sitemapUrls = sitemapJson.urlset.url.map(({ loc }) => {
+    return loc[0].replace(originRegex, '')
+  })
+
   it('can be opened', () => {
     const megaMenuDropdownSelector = '[data-test=content-menu-dropdown]'
     const megaMenuDropdownContentSelector =
@@ -13,5 +19,17 @@ describe('Mega menu dropdown', () => {
     cy.get(megaMenuDropdownSelector).click()
     cy.get(megaMenuDropdownSelector).should('be.visible')
     cy.get(megaMenuDropdownContentSelector).should('be.visible')
+  })
+
+  it('should contain correct links', () => {
+    // find all links in megamenu
+    cy.get('q-content-menu a').each($a => {
+      // skip the titles
+      if ($a.attr('class').indexOf('link_title') == -1) {
+        const url = $a.attr('href').replace(originRegex, '')
+        // url should match what is in the sitemap
+        expect(url).to.be.oneOf(sitemapUrls)
+      }
+    })
   })
 })
