eab946465e73719efced6eb6e24775dc1d948b04
Gregorio Iniesta <korgan00@gmail.com>
2021-12-15 12:03:22 +0100

    created components

---
+++ frontend/vue/components/CodeExercise/CodeEditorTools.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="code-editors-tools">
+    <button @click="copy">
+      Copy
+    </button>
+    <button @click="reset">
+      Reset
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+
+export default defineComponent({
+  name: 'CodeEditorTools',
+  components: {
+  },
+  methods: {
+    copy () {
+      console.log('copy')
+      this.$emit('copy')
+    },
+    reset () {
+      console.log('reset')
+      this.$emit('reset')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.code-exercise {
+}
+</style>
---
+++ frontend/vue/components/CodeExercise/CodeExercise.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="code-exercise">
+    <CodeEditor class="code-exercise__editor" @codeChanged="codeChanged" />
+    <ExerciseActionsBar class="code-exercise__actions-bar" @run="run" @grade="grade" />
+    <ExerciseResult class="code-exercise__result" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+import CodeEditor from './CodeEditor.vue'
+import ExerciseActionsBar from './ExerciseActionsBar.vue'
+import ExerciseResult from './ExerciseResult.vue'
+
+export default defineComponent({
+  name: 'CodeExercise',
+  components: {
+    CodeEditor,
+    ExerciseActionsBar,
+    ExerciseResult
+  },
+  props: {
+    code: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  methods: {
+    run () {
+      console.log('run from exercise')
+    },
+    grade () {
+      console.log('grade from exercise')
+    },
+    codeChanged () {
+      console.log('codeChanged from exercise')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.code-exercise {
+}
+</style>
