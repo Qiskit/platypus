f9ec09fb15057587628d898c7489a85038b12fb0
Gregorio Iniesta <korgan00@gmail.com>
2021-12-17 13:12:58 +0100

    added jupyterlab

--- frontend/vue/components/CodeExercise/CodeExercise.vue
+++ frontend/vue/components/CodeExercise/CodeExercise.vue
@@ -12,6 +12,47 @@ import CodeEditor from './CodeEditor.vue'
 import ExerciseActionsBar from './ExerciseActionsBar.vue'
 import ExerciseResult from './ExerciseResult.vue'
 
+const serverOptions = {
+  bootstrap: false,
+  preRenderHook: false,
+  stripPrompts: false,
+  requestKernel: true,
+  predefinedOutput: true,
+  mountStatusWidget: true,
+  mountActivateWidget: true,
+  mathjaxUrl: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js',
+  mathjaxConfig: 'TeX-AMS_CHTML-full,Safe',
+  selector: '[data-executable]',
+  outputSelector: '[data-executable-output]',
+  mountRunButton: true,
+  mountRestartButton: true,
+  mountRestartallButton: true,
+  binderOptions: {
+    repo: 'qiskit-community/platypus',
+    ref: 'binder-env',
+    binderUrl: 'https://mybinder.org',
+    savedSession: {
+      enabled: true,
+      maxAge: 86400,
+      storagePrefix: 'thebe-binder-'
+    }
+  },
+  codeMirrorconfig: {
+    lineWrapping: false
+  },
+  codeMirrorConfig: {
+    theme: 'abcdef',
+    mode: 'python'
+  },
+  kernelOptions: {
+    kernelName: 'python3',
+    path: '.',
+    serverSettings: {
+      appendToken: true
+    }
+  }
+}
+
 export default defineComponent({
   name: 'CodeExercise',
   components: {
