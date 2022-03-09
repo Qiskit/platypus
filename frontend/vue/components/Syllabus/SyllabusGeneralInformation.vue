3788e89516a4c98aa964d4611ab1f3ac3427064c
Gregorio Iniesta <korgan00@gmail.com>
2022-03-09 19:53:10 +0100

    created first structure of syllabus view

---
+++ frontend/vue/components/Syllabus/SyllabusGeneralInformation.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="syllabus-general-information">
+    <div class="syllabus-general-information__item">
+      <span class="syllabus-general-information__heading">Instructor:</span> <slot name="instructor" />
+    </div>
+    <div class="syllabus-general-information">
+      <span class="syllabus-general-information__heading">Location:</span> <slot name="location" />
+    </div>
+    <div class="syllabus-general-information">
+      <span class="syllabus-general-information__heading">University:</span> <slot name="university" />
+    </div>
+    <div class="syllabus-general-information">
+      <span class="syllabus-general-information__heading">Office Hours:</span> <slot name="office-hours" />
+    </div>
+    <div class="syllabus-general-information">
+      <span class="syllabus-general-information__heading">Class Hours:</span> <slot name="class-hours" />
+    </div>
+    <div class="syllabus-general-information">
+      <span class="syllabus-general-information__heading">Email:</span> <slot name="email" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+
+export default defineComponent({
+  name: 'SyllabusGeneralInformation'
+})
+</script>
+
+<style lang="scss" scoped>
+@import 'carbon-components/scss/globals/scss/typography';
+@import '~/../scss/variables/mq.scss';
+@import '~/../scss/mixins/mixins.scss';
+
+.syllabus-general-information {
+  @include type-style('body-long-01');
+  grid-area: data;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  gap: $spacing-06;
+
+  @include mq($until: large) {
+    grid-template-columns: auto;
+  }
+
+  &__item {
+    &__heading {
+      @include type-style('productive-heading-01');
+    }
+  }
+}
+</style>
---
+++ frontend/vue/components/Syllabus/SyllabusView.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="syllabus-view">
+    <h1>PHYS 332: Quantum Mechanics II (Spring, 2022)</h1>
+    <div class="syllabus-view__general-information">
+    </div>
+    <div class="syllabus-view__course-information">
+    </div>
+    <div class="syllabus-view__work-package">
+      <div class="syllabus-view__work-package__title">
+      </div>
+      <div class="syllabus-view__work-package__chapter-list">
+        <div class="syllabus-view__work-package__chapter-list__chapter">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+import UserAccountSectionHeader from '../UserAccount/UserAccountSectionHeader.vue'
+
+export default defineComponent({
+  name: 'SyllabusView',
+  components: {
+    UserAccountSectionHeader
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import 'carbon-components/scss/globals/scss/typography';
+@import '~/../scss/variables/mq.scss';
+@import '~/../scss/mixins/mixins.scss';
+
+.syllabus {
+  @include contained();
+  padding-top: $spacing-07;
+  padding-bottom: $spacing-07;
+
+  &__content {
+    padding: $spacing-07 0;
+  }
+}
+</style>
