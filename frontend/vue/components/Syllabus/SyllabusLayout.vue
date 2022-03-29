13df7e84cff77fe7539a49fe9c0b447d8d68301b
Gregorio Iniesta <korgan00@gmail.com>
2022-03-29 18:06:58 +0200

    mock data deleted

--- frontend/vue/components/Syllabus/SyllabusLayout.vue
+++ frontend/vue/components/Syllabus/SyllabusLayout.vue
@@ -28,37 +28,6 @@ export default defineComponent({
   data () {
     return {
       syllabus: {} as Syllabus
-      // TODO replace mock data
-      /*
-      syllabus: {
-        name: 'PHYS 332: Quantum Mechanics II (Spring, 2022)',
-        instructor: 'instructor name',
-        location: 'Madrid',
-        institution: 'UCM',
-        officeHours: '10:00 to 13:00',
-        classHours: '15:00 to 18:00',
-        email: 'none@none.never',
-        courseList: [
-          {
-            title: 'Week 1',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula tellus non ligula hendrerit interdum. Suspendisse sit amet erat vitae urna mattis sodales. Nullam consequat sagittis tellus. In et justo ex. Suspendisse tempor auctor blandit. Sed vel est eu felis vehicula varius id non ante. Morbi lacinia dolor ac nunc malesuada, dictum imperdiet ligula pellentesque.',
-            unitList: [
-              'bdb1d662-e0f6-428a-8830-befe6b47f320',
-              '7c765036-aebc-11ec-b909-0242ac120002',
-              '9260a554-aebc-11ec-b909-0242ac120002'
-            ]
-          },
-          {
-            title: 'Week 2',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula tellus non ligula hendrerit interdum. Suspendisse sit amet erat vitae urna mattis sodales. Nullam consequat sagittis tellus. In et justo ex. Suspendisse tempor auctor blandit. Sed vel est eu felis vehicula varius id non ante. Morbi lacinia dolor ac nunc malesuada, dictum imperdiet ligula pellentesque.',
-            unitList: [
-              '9e8ceb9e-aebc-11ec-b909-0242ac120002',
-              'a2e6f7a2-aebc-11ec-b909-0242ac120002'
-            ]
-          }
-        ]
-      } as Syllabus
-      */
     }
   },
   mounted() {
