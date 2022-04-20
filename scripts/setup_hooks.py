09d599658975ff19c0d3ea31c8b383d7d7c70d0c
Frank Harkins <frankharkins@hotmail.co.uk>
2022-04-20 12:24:32 +0100

    Add git hooks on install

---
+++ scripts/setup_hooks.py
@@ -0,0 +1,31 @@
+import os, sys, re
+
+HOOK_PATH = './.git/hooks/pre-commit'
+
+SCRIPT = """
+
+npm run --silent test:nb:meta || {
+  echo
+  echo "Notebook metadata / images are not optimized."
+  echo "Run 'npm run test:nb:fix' to fix, or use"
+  echo "'--no-verify' to ignore pre-commit hooks."
+  exit 1
+}
+
+"""
+
+if __name__ == '__main__':
+    if not os.path.isfile(HOOK_PATH):
+            pre_comments = '#!/bin/sh'
+            current_hook = ''
+    else:
+        with open(HOOK_PATH, 'r') as file:
+            contents = file.read()
+            if 'test:nb:meta' in contents:
+                print('pre-commit hook already set up.')
+                sys.exit(0)
+            pre_comments, hook = re.split(r'\n *[^#]', contents, 1)
+
+    new_hook = pre_comments + SCRIPT + hook
+    with open(HOOK_PATH, 'w+') as file:
+        file.write(new_hook)
