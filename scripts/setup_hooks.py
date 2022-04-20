d43d565bcef34779f2e305d58bba6975d7a64667
Frank Harkins <frankharkins@hotmail.co.uk>
2022-04-20 13:02:16 +0100

    Fix bug / rename variable

--- scripts/setup_hooks.py
+++ scripts/setup_hooks.py
@@ -17,15 +17,15 @@ npm run --silent test:nb:meta || {
 if __name__ == '__main__':
     if not os.path.isfile(HOOK_PATH):
             pre_comments = '#!/bin/sh'
-            current_hook = ''
+            old_hook = ''
     else:
         with open(HOOK_PATH, 'r') as file:
             contents = file.read()
             if 'test:nb:meta' in contents:
                 print('pre-commit hook already set up.')
                 sys.exit(0)
-            pre_comments, hook = re.split(r'\n *[^#]', contents, 1)
+            pre_comments, old_hook = re.split(r'\n *[^#]', contents, 1)
 
-    new_hook = pre_comments + SCRIPT + hook
+    new_hook = pre_comments + SCRIPT + old_hook
     with open(HOOK_PATH, 'w+') as file:
         file.write(new_hook)
