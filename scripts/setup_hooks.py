f1556d277c6cdfae523e2df9c8dd5d5c79867416
Frank Harkins <frankharkins@hotmail.co.uk>
2022-04-21 10:06:43 +0100

    Revert "debugging"
    This reverts commit a6c65af3be84d8591c18988eb634302e703172cc.

--- scripts/setup_hooks.py
+++ scripts/setup_hooks.py
@@ -16,19 +16,16 @@ npm run --silent test:nb:meta || {
 
 if __name__ == '__main__':
     try:
-        try:
-            with open(HOOK_PATH, 'r') as file:
-                contents = file.read()
-                if 'test:nb:meta' in contents:
-                    print('pre-commit hook already set up.')
-                    sys.exit(0)
-                pre_comments, old_hook = re.split(r'\n *[^#]', contents, 1)
-        except IOError:
-            pre_comments = '#!/bin/sh'
-            old_hook = ''
+        with open(HOOK_PATH, 'r') as file:
+            contents = file.read()
+            if 'test:nb:meta' in contents:
+                print('pre-commit hook already set up.')
+                sys.exit(0)
+            pre_comments, old_hook = re.split(r'\n *[^#]', contents, 1)
+    except IOError:
+        pre_comments = '#!/bin/sh'
+        old_hook = ''
 
-        new_hook = pre_comments + SCRIPT + old_hook
-        with open(HOOK_PATH, 'w+') as file:
-            file.write(new_hook)
-    except Exception as err:
-        print(err)
+    new_hook = pre_comments + SCRIPT + old_hook
+    with open(HOOK_PATH, 'w+') as file:
+        file.write(new_hook)
