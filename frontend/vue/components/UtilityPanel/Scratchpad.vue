d9036df5a2dd6168b47d422da0e959bae1566f1a
techtolentino <makertolentino@gmail.com>
2022-05-26 15:14:50 -0700

    add scratchpad helper text

--- frontend/vue/components/UtilityPanel/Scratchpad.vue
+++ frontend/vue/components/UtilityPanel/Scratchpad.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="scratchpad">
-    Scratchpad
+    <p class="scratchpad__description">This is your python scratchpad. Use this however you like while you work or copy the code over from the chapter.</p>
+    <p class="scratchpad__description"><strong>Note:</strong> Code in the scratchpad will not be saved.</p>
   </div>
 </template>
 
@@ -11,3 +12,14 @@ import { Vue, Options } from 'vue-class-component'
 
 export default class Scratchpad extends Vue {}
 </script>
+
+<style scoped lang="scss">
+@import 'carbon-components/scss/globals/scss/typography';
+
+.scratchpad {
+  &__description {
+    @include type-style('body-short-01');
+    margin-bottom: $spacing-05;
+  }
+}
+</style>
