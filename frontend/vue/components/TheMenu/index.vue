55b66a1e95c836ad8adf0f27b148a9da8eae6ca3
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2022-09-14 16:25:12 -0700

    feat: add click event tracking

--- frontend/vue/components/TheMenu/index.vue
+++ frontend/vue/components/TheMenu/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <qiskit-ui-shell />
+  <qiskit-ui-shell @on-click="onClick" />
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-class-component'
 import '@qiskit/web-components/components/ui-shell'
 
-export default class TheMenu extends Vue {}
+export default class TheMenu extends Vue {
+  onClick (e: CustomEvent) {
+    const windowInstance = (window as any)
+    windowInstance.textbook.trackClickEvent(`${e.detail.label} | ${e.detail.url}`, 'navbar')
+  }
+}
 </script>
