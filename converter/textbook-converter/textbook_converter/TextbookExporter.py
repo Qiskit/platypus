6035a58513594c147adf1bad1f04c3724b446141
Frank Harkins <frankharkins@hotmail.co.uk>
2022-10-05 16:44:07 +0100

    Handle code in headings

--- converter/textbook-converter/textbook_converter/TextbookExporter.py
+++ converter/textbook-converter/textbook_converter/TextbookExporter.py
@@ -204,8 +204,8 @@ def handle_hero_image(hero_image_syntax):
 def handle_heading(heading_syntax, in_block, suffix, section, is_problem_set=False):
     """Increase header level and compute level, title, and id"""
     header, title = heading_syntax.split(" ", 1)
+    title = handle_inline_code(title)
     level = header.count("#")
-    is_problem_set = is_problem_set
     if in_block:
         return None, None, title, f"#{heading_syntax}\n"
     else:
