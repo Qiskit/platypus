e41a45c881475abb02f3df35758377a8e288f99a
Frank Harkins <frankharkins@hotmail.co.uk>
2023-01-19 02:25:58 +0000

    Handle display equations in blockquotes (#1832)

--- converter/textbook-converter/textbook_converter/TextbookExporter.py
+++ converter/textbook-converter/textbook_converter/TextbookExporter.py
@@ -262,8 +262,9 @@ def handle_markdown_cell(cell, resources, cell_number, is_problem_set=False):
                 markdown_lines.append("\n")
                 in_latex = True
             continue
-        if line.lstrip().startswith("$$"):
+        if line.lstrip(' >').startswith("$$"):
             indent, l = line.split("$$", 1)
+            indent = indent.replace('>', '')
             assert not indent or indent.isspace()
             markdown_lines.append(f"{indent}```latex\n")
             if l.rstrip(" .").endswith("$$"):
@@ -292,7 +293,7 @@ def handle_markdown_cell(cell, resources, cell_number, is_problem_set=False):
             continue
 
         if in_blockquote:
-            if not line.startswith(">"):
+            if not line.startswith(">") and not in_latex:
                 in_blockquote = False
                 markdown_lines.append("</blockquote>\n")
         if line.startswith(">"):
